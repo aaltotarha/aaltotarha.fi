166fc9db62ac7b61ea2f5d25ac370a01ae20a44d
Iiro Jäppinen <iiro@jappinen.fi>
2020-03-22 15:09:12 +0200

    feat: add placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,54 @@
 import './app/globals'
 
+import { css } from 'linaria'
+
 import React from 'react'
 
-const App = () => <p>Hello, world!</p>
+const mainClass = css`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  padding: 2rem 1rem;
+`
+
+const headerClass = css`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  text-align: center;
+
+  h1 {
+    margin: auto;
+  }
+`
+
+const footerClass = css`
+  justify-content: center;
+  display: flex;
+  margin-top: auto;
+  text-align: center;
+
+  > * {
+    margin: 0.5rem;
+  }
+`
+
+const currentYear = new Date().getFullYear()
+
+const App = () => (
+  <main className={mainClass}>
+    <header className={headerClass}>
+      <h1>Viritetään...</h1>
+    </header>
+    <footer className={footerClass}>
+      <p>© {currentYear} Aaltotarha</p>
+      <aside>
+        <a href="https://github.com/aaltotarha/aaltotarha.fi" rel="author">
+          GitHub
+        </a>
+      </aside>
+    </footer>
+  </main>
+)
 
 export default App
--- src/app/globals.js
+++ src/app/globals.js
@@ -1,8 +1,42 @@
 import { css } from 'linaria'
-import reset from 'react-style-reset/string'
 
 css`
   :global() {
-    ${reset}
+    html,
+    body {
+      height: 100%;
+      margin: 0;
+    }
+
+    body {
+      background-color: hsla(0, 0%, 13%, 1);
+      color: hsla(0, 0%, 100%, 1);
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+        Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+        'Segoe UI Symbol';
+      font-size: 16px;
+    }
+
+    #root {
+      display: flex;
+      flex-direction: column;
+      min-height: 100%;
+    }
+
+    a {
+      color: hsla(0, 0%, 100%, 1);
+
+      &:hover {
+        color: hsla(0, 0%, 80%, 1);
+      }
+
+      &:active {
+        color: hsla(0, 0%, 60%, 1);
+      }
+
+      &:visited {
+        text-decoration: line-through;
+      }
+    }
   }
 `
